6bc9584d82a8642a0560419de9b1039e0f1a3dc6
pedro <pnarabelo@gmail.com>
2021-12-04 11:54:57 -0300

    Criação do frontend

---
+++ frontend/src/private/Settings/Settings.js
@@ -0,0 +1,14 @@
+
+
+function Settings() {
+
+  return (
+    <div className='d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4'>
+      <div className='d-block mb-4 mb-md-0'>
+        <h1 className='h4'>Configurações</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Settings;
---
+++ frontend/src/routes.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Route,
+  BrowserRouter,
+  Redirect,
+} from 'react-router-dom';
+import Login from './public/Login/Login';
+import Settings from "./private/Settings/Settings";
+
+function Routes() {
+  // function PrivateRoute({ children, ...rest }) {
+  //   return (
+  //     <Route
+  //       {...rest}
+  //       render={() => {
+  //         return localStorage.getItem('token') ? (
+  //           children
+  //         ) : (
+  //           <Redirect to='/' />
+  //         );
+  //       }}
+  //     />
+  //   );
+  // }
+
+  return (
+    <BrowserRouter>
+      <Route path='/' exact>
+        <Login />
+      </Route>
+      <Route path='/settings' exact>
+        <Settings />
+      </Route>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;

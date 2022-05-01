3bd486c25405f943be5a18d12bd8c6128df6c39f
pedro <prabelo@asert.com.br>
2022-05-01 12:12:19 -0300

    Monitor modal

---
+++ frontend/src/components/SwitchInput/SwitchInput.js
@@ -0,0 +1,33 @@
+/**
+ * props:
+ * - id
+ * - text
+ * - isChecked
+ * - onChange
+ */
+
+import { useEffect, useRef } from "react";
+
+function SwitchInput(props) {
+
+  const switchRef = useRef('');
+
+  function onChange(event) {
+    props.onChange({ target: { id: props.id, value: switchRef.current.checked } });
+  }
+
+  useEffect(() => {
+    switchRef.current.checked = props.isChecked;
+  }, [props.isChecked])
+
+  return (
+    <div>
+      <div className="form-check form-switch">
+        <input ref={switchRef} className="form-check-input" type="checkbox" id={props.id} onChange={onChange} />
+        <label htmlFor={props.id} className="form-check-label">{props.text}</label>
+      </div>
+    </div>
+  )
+}
+
+export default SwitchInput;
